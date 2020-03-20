6b0343a9f860d98192506baa17bcf60471281582
Shinatip T <shinatip.t@gmail.com>
2020-03-20 19:09:09 +0700

    Insurance selecting microservices

---
+++ src/main/java/com/sins/termproject/selectinginsurance/SelectingInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.sins.termproject.selectinginsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectingInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectingInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sins/termproject/selectinginsuranceweb/SelectingInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sins.termproject.selectinginsuranceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectingInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
